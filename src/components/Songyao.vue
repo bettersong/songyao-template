36a4c8ad7ea217fb83a30f56e3225897d7a64d94
宋瑶 <songyao@qutoutiao.net>
2019-11-11 16:05:51 +0800

    v1.0.1

--- src/components/Songyao.vue
+++ src/components/Songyao.vue
@@ -1,10 +1,10 @@
 <template>
-    <div>
+    <div class="songyao">
         <h1>{{ username }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
+      了解脚手架及脚手架指令请移步个人博客<br>
       check out the
-      <a href="http://47.100.126.169/zmengBlog" target="_blank" rel="noopener">vue-cli documentation</a>.
+      <a href="http://47.100.126.169/zmengBlog" target="_blank" rel="noopener">逐梦博客</a>.
     </p>
     </div>
 </template>
@@ -18,7 +18,8 @@ export default {
         }
     },
     created() {
-        window.console.log(`%c ${this.username}.`, 'color: #fff; background-image: repeating-radial-gradient(circle at 30% 60%,#99CCCC 20px, #7171B7 20px, #7171B7 20px, #CCCC99 40px, #CCCC99 60px, #4F9C9C 30px, #4F9C9C 40px, #99CCCC 80px, #99CCCC 50px); font-size: 16px;border-radius:15px 0;padding:5px;');
+        // console.log(this.publicPath)
+        window.console.log(`%c ${this.username} `, 'color: #fff; background-image: repeating-radial-gradient(circle at 30% 60%,#99CCCC 20px, #7171B7 20px, #7171B7 20px, #CCCC99 40px, #CCCC99 60px, #4F9C9C 30px, #4F9C9C 40px, #99CCCC 80px, #99CCCC 50px); font-size: 16px;border-radius:15px 0;padding:5px;');
         window.console.log('脚手架使用教程请看个人博客：47.100.126.169/zmengBlog')
     }
 }
---
+++ src/router/index.js
@@ -0,0 +1,4 @@
+// import Router from 'vue-router'
+export default {
+
+}
